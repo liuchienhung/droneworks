ee5b5e3ec577190e9b1d0cccc87af0c0d0cab7b6
DESKTOP-1RD78J6\neil <neil@geosat.com.tw>
2023-03-28 13:24:18 +0800

    fixed main.js and index.js.

--- src/main.js
+++ src/main.js
@@ -1,40 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import HomePage from './components/HomePage.vue'
-import LoginPage from './components/LoginPage.vue'
-import DroneServiceApplication from './components/DroneServiceApplication.vue'
-import ProfilePage from './components/ProfilePage.vue'
-import AdminPage from './components/AdminPage.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      component: LoginPage
-    },
-    {
-      path: '/login',
-      component: LoginPage
-    },
-    {
-      path: '/home',
-      component: HomePage
-    },
-    {
-      path: '/service-application',
-      component: DroneServiceApplication
-    },    {
-      path: '/profile',
-      component: ProfilePage
-    },
-    {
-      path: '/admin',
-      component: AdminPage
-    }
-  ]
-})
+import router from './router'
 
 const app = createApp(App)
 app.use(router)
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/components/LoginPage.vue'
 import HomePage from '@/components/HomePage.vue'
 
-const routes = [
-  {
-    path: '/',
-    redirect: '/login'
-  },
-  {
-    path: '/login',
-    name: 'LoginPage',
-    component: LoginPage
-  },
-  {
-    path: '/home',
-    name: 'HomePage',
-    component: HomePage
-  }
-]
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'LoginPage',
+      component: LoginPage
+    },
+    {
+      path: '/home',
+      name: 'HomePage',
+      component: HomePage
+    }
+  ]
+})
+
+export default router
