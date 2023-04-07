5ccc0e4306171eb935f7b68137db42197bd167ee
DESKTOP-1RD78J6\neil <neil@geosat.com.tw>
2023-04-08 00:09:46 +0800

    Add AdminLTE template style.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'admin-lte/dist/css/adminlte.min.css'
+import 'admin-lte/dist/js/adminlte.min.js'
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 const app = createApp(App)
 app.use(router)
